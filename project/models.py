7083e31e9ecade4f9c4020ccde2b3d977afb65d0
nalouddin <nalouddin@gmail.com>
2023-03-03 09:44:23 +0500

    Review va Tag modeli qo'shildi

--- project/models.py
+++ project/models.py
@@ -11,7 +11,28 @@ class Project(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     vote_count = models.IntegerField(default=0)
     vote_ratio = models.IntegerField(default=0)
-    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
     def __str__(self):
         return self.title
+    
+class Review(models.Model):
+    VOTE_TYPE = (
+        ("+", "Ijobiy"),
+        ("-", "Salbiy")
+    )
+    body = models.TextField(default=' ')
+    value = models.CharField(max_length=50, choices=VOTE_TYPE)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.value
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
